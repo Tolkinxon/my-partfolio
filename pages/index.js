8946bd54ad64eaabefa2955caad21f504561abbb
Tolkinxon <krosstony639@gmail.com>
2023-03-09 23:33:24 +0500

    app worked

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tesla clone</title>
+        <meta name="Tolkinxon" content="about tesla company" />
+      </Head>
+    
+        <h1>hello world</h1>
+    
+    </>
+  )
+}
