5de3a8a5df5be657f740386e791341eeff10c44a
Tolkinxon <krosstony639@gmail.com>
2023-03-10 23:33:47 +0500

    color is fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import '../styles/navbar.module.css'
+import styles from '../styles/navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>Navbar</nav>
+    <nav className={styles.navbar}>
+      <div>
+        <a href='#'><span>01</span> About</a>
+        <a href='#'><span>02</span> Experience</a>
+        <a href='#'><span>03</span> Work</a>
+        <a href='#'><span>04</span> Contact</a>
+        
+
+
+
+
+      </div>
+    </nav>
   )
 }
 
