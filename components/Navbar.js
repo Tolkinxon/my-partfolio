4153129cbef9c5293f24755745a9aa42883d1f1f
Tolkinxon <krosstony639@gmail.com>
2023-03-10 23:58:16 +0500

    removed svg

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,21 +4,14 @@ import styles from '../styles/navbar.module.css'
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-    
 
+
+      
       <div>
-        <a href="#">
-          <span>01</span> About
-        </a>
-        <a href="#">
-          <span>02</span> Experience
-        </a>
-        <a href="#">
-          <span>03</span> Work
-        </a>
-        <a href="#">
-          <span>04</span> Contact
-        </a>
+        <a href='#'><span>01. </span> About</a>
+        <a href='#'><span>02. </span> Experience</a>
+        <a href='#'><span>03. </span> Work</a>
+        <a href='#'><span>04. </span> Contact</a>
         <button>Resume</button>
       </div>
     </nav>
