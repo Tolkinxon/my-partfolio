85687d99e4ebb4e8c159dbc85064b64eef8074d7
Tolkinxon <krosstony639@gmail.com>
2023-03-12 00:01:44 +0500

    adden some css codes

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,6 @@ export default function Home() {
       <Head>
         <title>my portfolio</title>
         <meta name="Tolkinxon" content="about tesla company" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-          integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
       </Head>
       <div className="container">
         <Navbar />
