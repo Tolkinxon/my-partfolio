efef62c7bddafa3decc10108ced22eae8fafa580
Tolkinxon <krosstony639@gmail.com>
2023-03-11 23:29:36 +0500

    adden new component icons

---
+++ components/Icons.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Icons = () => {
+  return (
+    <div className="icons">
+    <i class="fa-brands fa-square-github"></i>
+    <i class="fa-brands fa-square-instagram"></i>
+    <i class="fa-brands fa-square-twitter"></i>
+    <i class="fa-brands fa-linkedin"></i>
+    <i class="fa-brands fa-codepen"></i>
+  </div>
+  )
+}
+
+export default Icons
--- pages/index.js
+++ pages/index.js
@@ -18,13 +18,7 @@ export default function Home() {
       <div className="container">
         <Navbar />
 
-        <div className="icons">
-          <i class="fa-brands fa-square-github"></i>
-          <i class="fa-brands fa-square-instagram"></i>
-          <i class="fa-brands fa-square-twitter"></i>
-          <i class="fa-brands fa-linkedin"></i>
-          <i class="fa-brands fa-codepen"></i>
-        </div>
+     
       </div>
     </>
   )
