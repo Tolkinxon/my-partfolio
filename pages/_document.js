d11b16bb03046049cebda30ad1fb7f01675501a8
Tolkinxon <krosstony639@gmail.com>
2023-03-11 23:16:20 +0500

    added link

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome-iconpicker/3.2.0/js/fontawesome-iconpicker.min.js" />
       <Head />
       <body>
         <Main />
