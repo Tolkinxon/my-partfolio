6b9f071c498eb8754dfa771f6cab401a510bd3b8
Tolkinxon <krosstony639@gmail.com>
2023-03-12 22:32:32 +0500

    added content side

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import style from '../styles/headr.module.css'
 
 const Header = () => {
   return (
     <div className="header">
-      <p>Hi, my name is </p>
-      <h1>Soliyev Tolkinkhon</h1>
-      <h2>I build things for the web.</h2>
-      <p>
+      <p className={style.p}>Hi, my name is </p>
+      <h1 className={style.h1}>Soliyev Tolkinkhon</h1>
+      <h2 className={style.h2}>I build things for the web.</h2>
+      <p className={style.biography}>
         I am frontend developer and learner backend side web programming. 
         I want to be a fullstack programmer. Now I am learning how build beautiful and awesome 
         web sites. 
       </p>
-      <button>chek out my projects</button>
+      <button className='button'>chek out my projects</button>
     </div>
   )
-}
+} 
 
 export default Header
