c5ed17d97ba0adb632ba8d6762e6b26eba58516c
Tolkinxon <krosstony639@gmail.com>
2023-03-21 23:38:31 +0500

    added new additions

--- components/SomeThingIveBuild.js
+++ components/SomeThingIveBuild.js
@@ -5,8 +5,20 @@ const SomeThingIveBuild = () => {
   return (
     <div className={style.someThingIveBuild}>
       <h1>
+        {' '}
         03. <span>Some Things I've Built</span>
       </h1>
+      <div className={style.picture}></div>
+      <div className={style.content}>
+        <h2>Featured project</h2>
+        <h2>Spotlify profile</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores veniam, optio, qui magnam, facilis rem placeat modi incidunt saepe deleniti perspiciatis natus maiores soluta accusamus sed itaque vero illum ducimus.</p>
+        <ul>
+            <li>
+                next Js
+            </li>
+        </ul>
+      </div>
     </div>
   )
 }
