4cae8618582e5a57d2d6661d8e0443219f4067fe
Tolkinxon <krosstony639@gmail.com>
2023-03-21 23:33:54 +0500

    added new additions

--- components/SomeThingIveBuild.js
+++ components/SomeThingIveBuild.js
@@ -3,10 +3,10 @@ import style from '../styles/SomeThingIveBuild.module.css'
 
 const SomeThingIveBuild = () => {
   return (
-    <div className={style.someThingIveBuild}> 
-        
-    
-    
+    <div className={style.someThingIveBuild}>
+      <h1>
+        03. <span>Some Things I've Built</span>
+      </h1>
     </div>
   )
 }
