35faf66b71b983635cc155c0b1f4f84fd80cc318
Tolkinxon <krosstony639@gmail.com>
2023-03-24 01:33:50 +0500

    added styles loader

--- components/Loader.js
+++ components/Loader.js
@@ -5,9 +5,11 @@ const Loader = () => {
   return (
     <div className={style.loader}>
       <div className={style.box_container}>
-        <div className={style.box}> <div></div><div></div> </div>
         <div className={style.box}> <div></div><div></div>  </div>
         <div className={style.box}> <div></div><div></div>  </div>
+        <div className={style.box}> <div></div><div></div>  </div>
+        <div className={style.box}><h1>S</h1></div>
+
       </div>
     </div>
   )
