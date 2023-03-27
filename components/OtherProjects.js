0b1dce35be56420bee6fad2c4c9033a0cd8a0520
Tolkinxon <krosstony639@gmail.com>
2023-03-27 19:46:07 +0500

    added new css

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -8,31 +8,34 @@ const OtherProjects = () => {
       <span>
         <span></span>view the archive
       </span>
-      <div className={style.item}>
-        <div className={style.folder}></div>
-        <h1>Single application project</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-          delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
-        </p>
-        <ul className={style.technologies}>
-          <li>css</li>
-          <li>html</li>
-        </ul>
-      </div>
-      <div className={style.item}>
-        <div className={style.folder}></div>
-        <h1>Single application project</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-          delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
-        </p>
-        <ul className={style.technologies}>
-          <li>css</li>
-          <li>html</li>
-        </ul>
+
+      <div>
+          <div className={style.item}>
+            <div className={style.folder}></div>
+            <h1>Single application project</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+              delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
+            </p>
+            <ul className={style.technologies}>
+              <li>css</li>
+              <li>html</li>
+            </ul>
+          </div>
+          <div className={style.item}>
+            <div className={style.folder}></div>
+            <h1>Single application project</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+              delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
+            </p>
+            <ul className={style.technologies}>
+              <li>css</li>
+              <li>html</li>
+            </ul>
+          </div>
       </div>
     </div>
   )
