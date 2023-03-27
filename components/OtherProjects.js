c2383078ccfcf915a80cd5849f30f22cb9a5105b
Tolkinxon <krosstony639@gmail.com>
2023-03-27 19:34:09 +0500

    added new css

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -5,14 +5,33 @@ const OtherProjects = () => {
   return (
     <div className={style.OtherProjects}>
       <h1>Other Noteworthy Projects</h1>
-      <span><span></span>view the archive</span>
+      <span>
+        <span></span>view the archive
+      </span>
       <div className={style.item}>
         <div className={style.folder}></div>
         <h1>Single application project</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eligendi qui a iste maiores sapiente natus saepe ut? Illum unde, delectus sit beatae velit incidunt modi illo eum excepturi accusamus!</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+          delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
+        </p>
         <ul className={style.technologies}>
-            <li>css</li>
-            <li>html</li>
+          <li>css</li>
+          <li>html</li>
+        </ul>
+      </div>
+      <div className={style.item}>
+        <div className={style.folder}></div>
+        <h1>Single application project</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+          delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
+        </p>
+        <ul className={style.technologies}>
+          <li>css</li>
+          <li>html</li>
         </ul>
       </div>
     </div>
