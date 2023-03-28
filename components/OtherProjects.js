01bb07d5ef75fe60cac4d8ea312943d68c3e6c9e
Tolkinxon <krosstony639@gmail.com>
2023-03-28 19:33:05 +0500

    added use effect

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -1,12 +1,9 @@
 import React, { useEffect } from 'react'
 import style from '../styles/OtherProjects.module.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const OtherProjects = () => {
-
   const [data, setData] = useState({})
-
-  useEffect(() => {},[])
 
   return (
     <div className={style.OtherProjects}>
@@ -16,45 +13,48 @@ const OtherProjects = () => {
       </span>
 
       <div>
-          <div className={style.item}>
-            <div className={style.folder}></div>
-            <h1>Single application project</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-              delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
-            </p>
-            <ul className={style.technologies}>
-              <li>css</li>
-              <li>html</li>
-            </ul>
-          </div>
-          <div className={style.item}>
-            <div className={style.folder}></div>
-            <h1>Single application project</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-              delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
-            </p>
-            <ul className={style.technologies}>
-              <li>css</li>
-              <li>html</li>
-            </ul>
-          </div>
-          <div className={style.item}>
-            <div className={style.folder}></div>
-            <h1>Single application project</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-              delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
-            </p>
-            <ul className={style.technologies}>
-              <li>css</li>
-              <li>html</li>
-            </ul>
-          </div>
+        <div className={style.item}>
+          <div className={style.folder}></div>
+          <h1>Single application project</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+            delectus sit beatae velit incidunt modi illo eum excepturi
+            accusamus!
+          </p>
+          <ul className={style.technologies}>
+            <li>css</li>
+            <li>html</li>
+          </ul>
+        </div>
+        <div className={style.item}>
+          <div className={style.folder}></div>
+          <h1>Single application project</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+            delectus sit beatae velit incidunt modi illo eum excepturi
+            accusamus!
+          </p>
+          <ul className={style.technologies}>
+            <li>css</li>
+            <li>html</li>
+          </ul>
+        </div>
+        <div className={style.item}>
+          <div className={style.folder}></div>
+          <h1>Single application project</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+            delectus sit beatae velit incidunt modi illo eum excepturi
+            accusamus!
+          </p>
+          <ul className={style.technologies}>
+            <li>css</li>
+            <li>html</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
