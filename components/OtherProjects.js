9cec300d682b2671bf5aac4025217dfcb236577b
Tolkinxon <krosstony639@gmail.com>
2023-03-28 19:07:35 +0500

    added some changes

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -36,6 +36,19 @@ const OtherProjects = () => {
               <li>html</li>
             </ul>
           </div>
+          <div className={style.item}>
+            <div className={style.folder}></div>
+            <h1>Single application project</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+              delectus sit beatae velit incidunt modi illo eum excepturi accusamus!
+            </p>
+            <ul className={style.technologies}>
+              <li>css</li>
+              <li>html</li>
+            </ul>
+          </div>
       </div>
     </div>
   )
