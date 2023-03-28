dcb6bb25e480ee3dc6fcf2b198ad7ee6fc4cd771
Tolkinxon <krosstony639@gmail.com>
2023-03-28 19:35:33 +0500

    added use effect

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -2,8 +2,15 @@ import React, { useEffect } from 'react'
 import style from '../styles/OtherProjects.module.css'
 import { useState } from 'react'
 
+
 const OtherProjects = () => {
   const [data, setData] = useState({})
+
+  useEffect(() => {
+    setData({
+      
+    })
+  },[])
 
   return (
     <div className={style.OtherProjects}>
@@ -13,34 +20,6 @@ const OtherProjects = () => {
       </span>
 
       <div>
-        <div className={style.item}>
-          <div className={style.folder}></div>
-          <h1>Single application project</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-            delectus sit beatae velit incidunt modi illo eum excepturi
-            accusamus!
-          </p>
-          <ul className={style.technologies}>
-            <li>css</li>
-            <li>html</li>
-          </ul>
-        </div>
-        <div className={style.item}>
-          <div className={style.folder}></div>
-          <h1>Single application project</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-            delectus sit beatae velit incidunt modi illo eum excepturi
-            accusamus!
-          </p>
-          <ul className={style.technologies}>
-            <li>css</li>
-            <li>html</li>
-          </ul>
-        </div>
         <div className={style.item}>
           <div className={style.folder}></div>
           <h1>Single application project</h1>
