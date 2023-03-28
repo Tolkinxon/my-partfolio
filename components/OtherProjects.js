0eb65692d18a72dc3128d4455eb4dc126c65237a
Tolkinxon <krosstony639@gmail.com>
2023-03-28 19:38:33 +0500

    added use effect

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -8,7 +8,12 @@ const OtherProjects = () => {
 
   useEffect(() => {
     setData({
-      
+      nameProject: 'Single application project',
+      descriptionProject: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+      eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
+      delectus sit beatae velit incidunt modi illo eum excepturi
+      accusamus!'`,
+      usingTechnoligies: ['css', 'bootstrap']
     })
   },[])
 
@@ -22,16 +27,12 @@ const OtherProjects = () => {
       <div>
         <div className={style.item}>
           <div className={style.folder}></div>
-          <h1>Single application project</h1>
+          <h1>{}</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            eligendi qui a iste maiores sapiente natus saepe ut? Illum unde,
-            delectus sit beatae velit incidunt modi illo eum excepturi
-            accusamus!
+            {}
           </p>
           <ul className={style.technologies}>
-            <li>css</li>
-            <li>html</li>
+
           </ul>
         </div>
       </div>
