3fc3b1bdba30271d5f42d6e4ed656a8cc8cd5da2
Tolkinxon <krosstony639@gmail.com>
2023-03-31 20:06:25 +0500

    new changes

--- components/whatNext.js
+++ components/whatNext.js
@@ -4,7 +4,9 @@ import style from '../styles/whatNext.module.css'
 const whatNext = () => {
   return (
     <div className={style.whatNext}>
-        <div className={style.container}></div>
+      <div className={style.container}>
+        <h1>04. What's Next?</h1>
+      </div>
     </div>
   )
 }
