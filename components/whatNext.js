2184c680ebc5ee7eb8e0d5fefd416e7316a60b77
Tolkinxon <krosstony639@gmail.com>
2023-04-01 09:45:33 +0500

    last component

--- components/whatNext.js
+++ components/whatNext.js
@@ -6,6 +6,10 @@ const whatNext = () => {
     <div className={style.whatNext}>
       <div className={style.container}>
         <h1>04. What's Next?</h1>
+        <h2>Get In Touch</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, excepturi minus voluptatum sint error eaque aspernatur ea, voluptas illo facilis maxime aut corporis quos ullam deleniti aperiam animi officiis amet.
+        </p>
+        <button>Say Hello</button>
       </div>
     </div>
   )
