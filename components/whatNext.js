2dbd4ba6aa7eb7b840770545e1ac7cf71f72b14d
Tolkinxon <krosstony639@gmail.com>
2023-04-01 09:54:28 +0500

    last component

--- components/whatNext.js
+++ components/whatNext.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import style from '../styles/whatNext.module.css'
 
-const whatNext = () => {
+const WhatNext = () => {
   return (
     <div className={style.whatNext}>
       <div className={style.container}>
         <h1>04. What's Next?</h1>
         <h2>Get In Touch</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, excepturi minus voluptatum sint error eaque aspernatur ea, voluptas illo facilis maxime aut corporis quos ullam deleniti aperiam animi officiis amet.
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+          excepturi minus voluptatum sint error eaque aspernatur ea, voluptas
+          illo facilis maxime aut corporis quos ullam deleniti aperiam animi
+          officiis amet.
         </p>
         <button>Say Hello</button>
       </div>
@@ -15,4 +19,4 @@ const whatNext = () => {
   )
 }
 
-export default whatNext
+export default WhatNext
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import About from '../components/About'
 import Loader from '../components/Loader'
 import SomeThingIveBuild from '@/components/SomeThingIveBuild'
 import OtherProjects from '@/components/OtherProjects'
-import whatNext from '@/components/whatNext'
+import WhatNext from '@/components/whatNext'
 import { useRef, useState, useEffect } from 'react'
 import Experience from '@/components/Experience'
 
@@ -55,7 +55,7 @@ export default function Home() {
             <Experience />
             <SomeThingIveBuild />
             <OtherProjects />
-            <whatNext />
+            <WhatNext />
           </div>
         </div>
       )}
