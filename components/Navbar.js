32a7cff50b9decaee8b432784b3d3395da5df6b6
Tolkinxon <krosstony639@gmail.com>
2023-04-11 18:33:38 +0500

    added new wrapper

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,28 +10,31 @@ const Navbar = ({ scroll }) => {
         <div className={styles.box}></div>
         <div className={styles.box}>S</div>
       </div>
-
+  <div className={styles.selects_wrapper}>
       <div className={styles.selects}>
-        <a href="#">
-          <span>01. </span> About
-        </a>
-        <a href="#">
-          <span>02. </span> Experience
-        </a>
-        <a href="#">
-          <span>03. </span> Work
-        </a>
-        <a href="#">
-          <span>04. </span> Contact
-        </a>
-        <button className="button">Resume</button>
-      </div>
+          <a href="#">
+            <span>01. </span> About
+          </a>
+          <a href="#">
+            <span>02. </span> Experience
+          </a>
+          <a href="#">
+            <span>03. </span> Work
+          </a>
+          <a href="#">
+            <span>04. </span> Contact
+          </a>
+          <button className="button">Resume</button>
+        </div>
+  </div>
+     
 
       <div className={styles.hide_selects}>
         <div></div>
         <div></div>
         <div></div>
       </div>
+
     </nav>
   )
 }
