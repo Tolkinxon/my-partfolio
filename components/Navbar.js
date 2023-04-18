7f088556b209fa95baafddac4b38796e0476f8a6
Tolkinxon <krosstony639@gmail.com>
2023-04-18 23:48:08 +0500

    added some changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../styles/navbar.module.css'
 
 const Navbar = ({ scroll }) => {
+  const [clicking, setClicking] = useState({})
+
 
 
   const hideAndShow = () => {
