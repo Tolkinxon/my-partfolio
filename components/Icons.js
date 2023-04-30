b990164d58f0cd3ef6e4905efbadae18f1c4fc9e
Tolkinxon <krosstony639@gmail.com>
2023-04-30 23:08:52 +0500

    some changes

--- components/Icons.js
+++ components/Icons.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import style from '../styles/icons.module.css'
 
 const Icons = () => {
   return (
     <>
-      <div className="icons">
+      <div className={style.icons}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           role="img"
@@ -13,7 +14,6 @@ const Icons = () => {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="feather feather-github"
         >
           <title>GitHub</title>
           <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
@@ -27,7 +27,6 @@ const Icons = () => {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="feather feather-instagram"
         >
           <title>Instagram</title>
           <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
@@ -43,7 +42,6 @@ const Icons = () => {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="feather feather-twitter"
         >
           <title>Twitter</title>
           <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
@@ -57,7 +55,6 @@ const Icons = () => {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="feather feather-linkedin"
         >
           <title>LinkedIn</title>
           <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
@@ -73,7 +70,6 @@ const Icons = () => {
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
-          class="feather feather-codepen"
         >
           <title>CodePen</title>
           <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
@@ -82,12 +78,12 @@ const Icons = () => {
           <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
           <line x1="12" y1="2" x2="12" y2="8.5"></line>
         </svg>
-        <div className="line"></div>
+        <div className={style.line}></div>
       </div>
 
-      <span className="email">
+      <span className={style.email}>
         <span>soliyevtolkinxon@gmail.com</span>
-        <span className="lane"></span>
+        <span className={style.lane}></span>
       </span>
     </>
   )
