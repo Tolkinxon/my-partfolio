2bf281d389b28df2c466e4b1f86963ac739fa77a
Tolkinxon <krosstony639@gmail.com>
2024-05-02 10:07:19 +0300

    some changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <p className='p'>Hi, my name is </p>
+      <h1 className='h1'>Soliyev To'lqinxon</h1>
+      <h2 className='h2'>I build things for the web.</h2>
+      <p className='biography'>
+        I am frontend developer and learner backend side web programming. 
+        I want to be a fullstack programmer. Now I am learning how build beautiful and awesome 
+        web sites. 
+      </p>
+      <button className='button'>chek out my projects</button>
+    </div>
+  )
+} 
+
+export default Header
